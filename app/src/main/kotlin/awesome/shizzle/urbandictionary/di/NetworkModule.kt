a0b805970058c0895d1f5286fdc3dd9908b6b729
Akash <akash87@gmail.com>
2019-08-07 16:08:12 -0700

    adding caching interceptors for okhttp. need better logic check for interception

--- app/src/main/kotlin/awesome/shizzle/urbandictionary/di/NetworkModule.kt
+++ app/src/main/kotlin/awesome/shizzle/urbandictionary/di/NetworkModule.kt
@@ -1,20 +1,29 @@
 package awesome.shizzle.urbandictionary.di
 
+import awesome.shizzle.urbandictionary.network.CachingInterceptor
 import awesome.shizzle.urbandictionary.network.HeaderInterceptor
 import awesome.shizzle.urbandictionary.network.RapidAPIService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.Cache
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val networkModule = module {
-    single { OkHttpClient.Builder().addInterceptor(HeaderInterceptor()).build() }
     single {
+
+        val okClient = OkHttpClient.Builder()
+            .cache(Cache(androidContext().cacheDir, 5 * 1024 * 1024))
+            .addInterceptor(CachingInterceptor(androidContext()))
+            .addNetworkInterceptor(HeaderInterceptor())
+            .build()
+
         Retrofit.Builder()
             .baseUrl("https://mashape-community-urban-dictionary.p.rapidapi.com/")
-            .client(get())
+            .client(okClient)
             .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
             .build()
     }
---
+++ app/src/main/kotlin/awesome/shizzle/urbandictionary/network/CachingInterceptor.kt
@@ -0,0 +1,48 @@
+package awesome.shizzle.urbandictionary.network
+
+import android.content.Context
+import kotlinx.io.IOException
+import okhttp3.CacheControl
+import okhttp3.Interceptor
+import okhttp3.Request
+import java.util.concurrent.TimeUnit
+import android.net.NetworkInfo
+import android.net.ConnectivityManager
+import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
+
+
+class CachingInterceptor(context: Context) : Interceptor {
+    private val connectivityManager = context.getSystemService(
+        Context.CONNECTIVITY_SERVICE
+    ) as ConnectivityManager
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+        var request = chain.request()
+
+        request = Request.Builder()
+            .cacheControl(
+                CacheControl.Builder()
+                    .maxAge(1, TimeUnit.DAYS)
+                    .minFresh(4, TimeUnit.HOURS)
+                    .maxStale(8, TimeUnit.HOURS)
+                    .build()
+            )
+            .url(request.url())
+            .build()
+
+
+        return chain.proceed(request)
+    }
+
+    private fun isConnected(): Boolean {
+        try {
+            return connectivityManager.activeNetworkInfo.isConnected
+        } catch (e: Exception) {
+            Log.w("CachingInterceptor", e.toString())
+        }
+
+        return false
+    }
+}
